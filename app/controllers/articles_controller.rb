0ab7e2d3c9c8f1c9112741b6271e49281d5e051b
Adam Borek <adambor3k@gmail.com>
2018-10-09 14:24:26 +0200

    Adds routing for show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     render json: 'hello'
   end
+
+  def show
+    render json: params[:id]
+  end
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 describe 'articles routes' do
-  it 'should rout to articles index' do
+  it 'should route to articles index' do
     expect(get 'articles').to route_to 'articles#index'
+  end
+
+  context 'for given id' do
+    it 'should route to the article show action' do
+      expect(get 'articles/1').to route_to 'articles#show', id: '1'
+    end
   end
 end
